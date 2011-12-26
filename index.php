47bee474a901910df169aa17b23f38a761d2609f
saini <vaibps17@gmail.com>
2011-12-26 19:58:40 +0530

    added .gitignore file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = array();
+$a[0] = "asdddsas";
+$a[1] = "asdas";
+$a[2] = "asddd";
+$a[3] = "asasd";
+return json_encode($a);
+echo json_encode($a);
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
